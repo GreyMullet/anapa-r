44750eb0c7e9454b8938d70b596fc6003990484b
GreyMullet <ogonannikita52@gmail.com>
2025-08-06 12:14:11 +0300

    booking page added

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,22 @@
+export default function Booking(){
+    return(
+        <div
+            style={{
+                width: '100%',
+            }}
+        >
+            <iframe
+                title="Модуль бронирования"
+                src="https://cb08796.tw1.ru/hotel/94246ce346bbe6fb3db96c8176e76268a8642be5842ee76ba6b762253504c385"
+                style={{
+                    width: '100%',
+                    maxWidth: '1400px',
+                    height: '100vh',
+                    border: 'none',
+                    display: 'block',
+                    margin: '0 auto',
+                }}
+            ></iframe>
+        </div>
+    )
+}
